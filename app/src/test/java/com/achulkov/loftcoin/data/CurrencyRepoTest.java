61ec637607fd98254a0d315a1226ad1ea05823a1
Chulkov, Andrey <Andrey.Chulkov@nexign.com>
2020-12-25 09:47:05 +0300

    tests_added

---
+++ app/src/test/java/com/achulkov/loftcoin/data/CurrencyRepoTest.java
@@ -0,0 +1,13 @@
+package com.achulkov.loftcoin.data;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public class CurrencyRepoTest extends CurrencyRepoImpl {
+
+    public CurrencyRepoTest(@NonNull Context context) {
+        super(context);
+    }
+
+}
